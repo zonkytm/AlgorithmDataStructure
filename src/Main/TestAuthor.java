d0766323beb7fb4c707547e2759d3d0d9d75438e
zonky <Juphy@yandex.ru>
2022-09-09 10:23:53 +0300

    add main in testaurhor

--- src/Main/TestAuthor.java
+++ src/Main/TestAuthor.java
@@ -2,7 +2,10 @@ package Main;
 
 public class TestAuthor {
     private Author AuthorToTest;
+    public void main(String[]args){
+        System.out.println("v");
 
+    }
     public void setAuthorToTest(Author authorToTest) {
         AuthorToTest = authorToTest;
         System.out.println(AuthorToTest.getName()+" "+" "+AuthorToTest.getEmail()+" "+AuthorToTest.getGender());
